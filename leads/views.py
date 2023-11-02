642b9f26bfc6753b10bfa9ca63a3c7f320475c4a
Blaise <ejikemeblaise08@gmail.com>
2023-11-02 16:01:01 +0100

    'password-reset'

--- leads/views.py
+++ leads/views.py
@@ -40,39 +40,55 @@ class LeadsListView(LoginRequiredMixin, generic.ListView):
         if user.is_organizer:
             queryset = Lead.objects.filter(organization=user.userprofile)
         else:
-            queryset = Lead.objects.filter(organization=user.agent.organization)
+            queryset = Lead.objects.filter(
+                organization=user.agent.organization)
             # filter according n reassign the queryset
             # which doesnt make multiple queryset in the db
 
             # filter the agent user via deep filter call (agent__user: filters the lead based on the agent
             # field where that agent has a user == self.request.user )
-           
+
            # filter agent that is logged in
             queryset = queryset.filter(agent__user=self.request.user)
             # when returned django then evaluate what you filtered
-
-            
-       
         return queryset
 
+    def get_context_data(self, **kwargs):
+        """function that helps us to filter and split leads that have not been 
+        assigned yet to an agent """
+        
+        context = super(LeadsListView, self).get_context_data(**kwargs)
+        user = self.request.user
+        
+        if user.is_organizer:
+            # agent__isnull= True -> to check whether a foreign key is null.
+            queryset = Lead.objects.filter(
+                organization=user.userprofile, agent__isnull=True
+                )
+        
+            context.update({
+                  "unassigned_leads":queryset
+            })
+        
+        return context
 
 class LeadsCreateView(OrgnizerAndLoginRequiredMixin, generic.CreateView):
-    
+
     template_name = 'leads/lead-create.html'
     form_class = LeadModelForm
-    
+
     def get_queryset(self):
         organization = self.request.user.userprofile
         queryset = Lead.objects.filter(organization=organization)
         return queryset
-    
+
     def get_success_url(self):
         return reverse('leads:lead-list')
 
     def form_valid(self, form):
        # lead = Lead.objects.create(name="John", organization=org)
        # organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)
-       
+
         user = form.save(commit=False)
         # create lead user
         user.is_agent = False
@@ -84,7 +100,7 @@ class LeadsCreateView(OrgnizerAndLoginRequiredMixin, generic.CreateView):
             user=user,
             organization=self.request.user.userprofile
         )
-        
+
         # TODO send email
         send_mail(
             subject='A lead has been created',
@@ -105,11 +121,13 @@ class LeadsDetailView(OrgnizerAndLoginRequiredMixin, generic.DetailView):
         if user.is_organizer:
             queryset = Lead.objects.filter(organization=user.userprofile)
         else:
-            queryset = Lead.objects.filter(organization=user.agent.organization)
-            
+            queryset = Lead.objects.filter(
+                organization=user.agent.organization)
+
             queryset = queryset.filter(agent__user=self.request.user)
             # when returned django then evaluate what you filtered
         return queryset
+
 
 class LeadsUpdateView(OrgnizerAndLoginRequiredMixin, generic.UpdateView):
     template_name = "leads/lead-update.html"
@@ -120,24 +138,22 @@ class LeadsUpdateView(OrgnizerAndLoginRequiredMixin, generic.UpdateView):
     def get_success_url(self):
         return reverse('leads:home-page')
 
-    
     def get_queryset(self):
         user = self.request.user
         # login in user - an organizer?
         if user.is_organizer:
-            return Lead.objects.filter(organization=user.userprofile) 
-      
+            return Lead.objects.filter(organization=user.userprofile)
+
 
 class LeadsDeleteView(LoginRequiredMixin, generic.DeleteView):
     template_name = 'leads/lead-delete.html'
-   
-    
+
     def get_queryset(self):
         user = self.request.user
         # initial queryset for entire organization?
         queryset = Lead.objects.filter(organization=user.userprofile)
-        
+
         return queryset
-    
+
     def get_success_url(self):
         return reverse('leads:home-page')
--- musics/urls.py
+++ musics/urls.py
@@ -5,37 +5,42 @@ from django.contrib.auth.views import (
     LoginView,
     LogoutView,
     PasswordResetView,
-    
-    
-    )
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+
+)
 from django.views.generic import TemplateView
 from django.urls import path, include
-from leads.views import  LandingPageView, SignUpView
+from leads.views import LandingPageView, SignUpView
 
 admin.site.site_header = 'Django_Test Enterprise'
 admin.site.index_title = 'Django_Test Administration'
-
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    
-    #path('', landing_page, name="landing-page"),
+
+    # path('', landing_page, name="landing-page"),
     path('', LandingPageView.as_view(), name='landing-page'),
     path('song/', include('songs.urls', namespace='songs')),
     path('leads/', include("leads.urls", namespace='leads')),
     path("__reload__/", include("django_browser_reload.urls")),
     path("agents/", include("agents.urls", namespace='agents')),
-    
-    # Registra tion URL
+
+    # Registration URL - inherited for django.contrib.auths views
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
-    
-
+    path('password-reset-done/', PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 
 
 ]
